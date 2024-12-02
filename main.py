8dc665ce3b5861d5bfbfcc19f88c33b84db7ae0a
boom2306 <mohamedrawhy2005@gmail.com>
2024-12-02 22:20:45 +0200

    second commit
    Line 6 - assuming the bmp image is 24 bit, so 3 bytes per pixel
    
    Line 7 - converts the message into binary
    
    Line 8 - starting from the 54th byte, which is the start of the pixel data

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ def hide_message(image, message):
 
     with open(image, "r,b") as f:
         image_data = bytearray(f.read())
+
+    pixel = 3
+    message_bits = ''.join(format(ord(char), '08b') for char in message)
+    index = 54
