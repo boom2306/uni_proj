d5aaeccdcf1df3035a85c238530f3014cfdfbd38
boom2306 <mohamedrawhy2005@gmail.com>
2024-12-04 20:00:55 +0200

    third commit
    rest of the code

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ def hide_message(image, message):
     pixel = 3
     message_bits = ''.join(format(ord(char), '08b') for char in message)
     index = 54
+
+    for bit in message_bits:
+        for color_channel in range(pixel):
+            image_data[index + color_channel] &= 254
+            image_data[index + color_channel] |= int(bit)
+        index += pixel
+
+    with open("secret_image.bmp", "wb") as f:
+        f.write(image_data)
+
+image_path = "image.bmp"
+message = "secret"
+
+hide_message(image_path, message)
